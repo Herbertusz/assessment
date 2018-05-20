11017ff603edab1bb1f88dedae80505d9afcdedd
Herbertusz <horv.norbert@gmail.com>
2018-05-20 20:17:22 +0200

    Algorithm and algorithm tests

---
+++ js-numerals/js/test/numeral.js
@@ -0,0 +1,34 @@
+/* global describe it */
+
+const assert = require('assert');
+const {numberPartToString, numberToString} = require('../numeral.js');
+
+describe('Numeral', function(){
+
+    describe('numberPartToString', function(){
+
+        it('8',   () => { assert.equal(numberPartToString('8'),   'eight'); });
+        it('008', () => { assert.equal(numberPartToString('8'),   'eight'); });
+        it('12',  () => { assert.equal(numberPartToString('12'),  'twelve'); });
+        it('012', () => { assert.equal(numberPartToString('12'),  'twelve'); });
+        it('18',  () => { assert.equal(numberPartToString('18'),  'eighteen'); });
+        it('68',  () => { assert.equal(numberPartToString('68'),  'sixty-eight'); });
+        it('068', () => { assert.equal(numberPartToString('68'),  'sixty-eight'); });
+        it('100', () => { assert.equal(numberPartToString('100'), 'one hundred'); });
+        it('108', () => { assert.equal(numberPartToString('108'), 'one hundred and eight'); });
+        it('160', () => { assert.equal(numberPartToString('160'), 'one hundred and sixty'); });
+        it('168', () => { assert.equal(numberPartToString('168'), 'one hundred and sixty-eight'); });
+
+    });
+
+    describe('numberToString', function(){
+
+        it('7',     () => { assert.equal(numberToString('7'),     'seven'); });
+        it('42',    () => { assert.equal(numberToString('42'),    'forty-two'); });
+        it('2001',  () => { assert.equal(numberToString('2001'),  'two thousand and one'); });
+        it('1999',  () => { assert.equal(numberToString('1999'),  'nineteen hundred and ninety-nine'); });
+        it('17999', () => { assert.equal(numberToString('17999'), 'seventeen thousand nine hundred and ninety-nine'); });
+
+    });
+
+});
